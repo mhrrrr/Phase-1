714e282d5c99797d53e15be7bbd9e750f5b3a062
Sachchit Vekaria <sachchit.vekaria@generalaeronautics.com>
2020-07-22 22:01:39 +0530

    Added testutil.py which can be used for easy testing of common functionalities without touching the specific util files for each vehicle.

---
+++ util/testutil.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 14:07:56 2020
+
+@author: sachchit
+"""
+
+# import necessary modules
+import time
+from util.gacommonutil import CompanionComputer, mavutil
+import threading
+import logging
+
+class TestCompanionComputer(CompanionComputer):
+    def __init__(self, sitlType):
+        # Initialize super class
+        super().__init__(sitlType)
+        
+        # Threading Lock for TestCompanionComputer Class
+        self.lock = threading.Lock()
+        self.handleRecievedMsgThread = None
+        
+    def init(self):
+        super().init()
+        
+        # set data stream rate
+        self.set_data_stream()
+        
+        # start our recieving message handling loop
+        self.handleRecievedMsgThread = threading.Thread(target=self.handle_recieved_message)
+        self.handleRecievedMsgThread.start()
+        
+        while True:
+            time.sleep(1)
+            
+    def set_data_stream(self):
+        # data rate of more than 100 Hz is not possible as recieving loop is set to run at interval of 0.01 sec
+        # don't change that as it affects other vehicles as well
+    
+        # request data to be sent at the given rate
+        # arguments are (target_system, target_component, stream_type, frequency in Hz, stop(0) or start(1))
+    
+        # stop data which are coming automatically to stop recieving unnecessary messeges
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 4, 0))
+
+
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RAW_SENSORS, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTENDED_STATUS, 2, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RC_CHANNELS, 2, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RAW_CONTROLLER, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_POSITION, 5, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA1, 1, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA2, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA3, 2, 1))
+    
+        logging.info("Stream Rate have been set")
+    
+    def handle_recieved_message(self):
+        while True:
+            time.sleep(0.01)
+            if self.killAllThread.is_set():
+                break
+            recievedMsg = self.get_new_message_from_recieving_queue()
+            if recievedMsg is not None:
+                super().handle_recieved_message(recievedMsg)
+            
+    def kill_all_threads(self):
+        logging.info("TestCompanionComputer killing all threads")
+        super().kill_all_threads()
+#        self.killAllThread.set()
+#        
+#        for task in self.scheduledTaskList:
+#            task.stop()
+#        
+        self.handleRecievedMsgThread.join()
+        logging.info("TestCompanionComputer joined all threads")
